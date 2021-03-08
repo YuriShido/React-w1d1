8c9f76eb437d8c307daac75299adcc3d93695348
YuriShido <sydonce57@gmail.com>
2021-03-08 13:26:34 -0800

    first commit

--- 06-practice-class/Category.js
+++ 06-practice-class/Category.js
@@ -1,11 +1,30 @@
-// change this Component to a class!
-function Category(props){
-    // get data from props and use map to build an array of <li>
-    // ... code goes here
-    return(
-        <div>
-            {/* Your Code Here */}
-        </div>
-    )
+
+class Category extends React.Component {
+
+    constructor() {
+        super()
+    }
+    
+    render() {
+        let categories = data.map((result, index) => {
+            console.log(result)
+             return (
+                <li class="cat-link left valign-wrapper" key={index}>
+                <i class="material-icons">{result.icon}</i>{result.title}
+               </li>
+               
+             )
+          })
+
+        return(
+            <div className="row">
+                <ul class="cat-nav center-align">
+                    {categories}
+                </ul>
+            </div>
+        )
+    }
 }
+
+
 
